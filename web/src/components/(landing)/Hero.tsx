dbf87d79d2d165e735b5a6bea3a6f46774563959
AaryanNarayani <aaryannarayani2004@gmail.com>
2025-10-19 17:32:34 +0530

    feat: added landing mock video for now

--- web/src/components/(landing)/Hero.tsx
+++ web/src/components/(landing)/Hero.tsx
@@ -3,8 +3,21 @@ import { Link } from 'react-router-dom';
 
 export default function Hero() {
     return(
-        <div className="relative min-h-screen flex items-center justify-center bg-transparent text-center z-10">
-            <div className="max-w-3xl mx-auto px-4 -mt-24">
+        <div className="relative min-h-screen flex items-center justify-center bg-transparent text-center overflow-hidden">
+            <video
+                autoPlay
+                muted
+                playsInline
+                className="absolute top-0 left-0 w-full h-full object-cover z-0"
+            >
+                <source src="/octaknight.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+
+            <div className="absolute top-0 left-0 w-full h-full bg-black/50 z-5"></div>
+
+            {/* Your content (z-10) */}
+            <div className="relative max-w-3xl mx-auto px-4 -mt-24 z-10">
                 <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bricolage text-white leading-snug animate-fade-in-up">
                     Engineering the Next <br />
                     <span className="text-[var(--color-primary-400)]">Industrial Revolution</span>
@@ -20,12 +33,14 @@ export default function Hero() {
                     >
                         Request demo
                     </button>
-                    <Link to={"/tool-management"} 
+                    
+                    <Link 
+                        to={"/tool-management"} 
                         className="cursor-pointer px-8 py-3 bg-[var(--color-primary-400)] rounded-full font-satoshi text-[var(--secondary)] font-medium hover:bg-[var(--color-primary-300)] transition-all duration-300 shadow-lg shadow-yellow-500/10 flex items-center justify-center"
                     >
-                        <Link to="/tool-management" className="whitespace-nowrap mr-2 text-black">
+                        <span className="whitespace-nowrap mr-2 text-black">
                             Explore solutions
-                        </Link>
+                        </span>
                         <div
                             className="flex items-center justify-center h-6 w-6 bg-black rounded-full text-[var(--color-primary-400)]"
                         >
@@ -37,4 +52,3 @@ export default function Hero() {
         </div>
     )
 }
-
