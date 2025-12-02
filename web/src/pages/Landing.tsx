2a77edcef9f6fc97ef2f2d920a914c3fa925736d
AaryanNarayani <aaryannarayani2004@gmail.com>
2025-12-02 17:25:03 +0530

    build: fixes

--- web/src/pages/Landing.tsx
+++ web/src/pages/Landing.tsx
@@ -1,7 +1,6 @@
 import Featured from "@/components/(landing)/Featured";
 import Hero from "@/components/(landing)/Hero";
 import Solutions from "@/components/(landing)/Solutions";
-import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 
 export default function Landing(){
